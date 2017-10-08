e1f575231edac2f454650a9afb4e7e629e269364
Mehmet Dogan <info@intelligentworks.ch>
2017-10-08 19:33:03 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/**
+ * main.js is the entry point to your program.
+ * You find the program logic, data structures and function calls in this file 
+ */
+
+"use strict";
+
+// Please us test.js for this assignment
---
+++ test/tests.js
@@ -0,0 +1,11 @@
+/**
+ * Please use the file "js/functions.js" for your functions.
+ *
+ */
+
+
+describe('Test Suite - Let\'s use jquery', () => {
+    
+    
+});
+
