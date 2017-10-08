9e9dc47cf44dbb5be54028562926102b7d817047
Mehmet Dogan <info@intelligentworks.ch>
2017-10-08 20:00:03 +0200

    add jquery tests

--- test/tests.js
+++ test/tests.js
@@ -6,6 +6,62 @@
 
 describe('Test Suite - Let\'s use jquery', () => {
     
+    it('should hide all the images from the view', () =>{
+        
+    });
+    
+    it('should show all the images again', () =>{
+
+    });
+    
+    it('should change the heading to "The Best Collection"', () =>{
+
+    }); 
+    
+    it('should make the hr (.line) element bolder', () =>{
+
+    }); 
+    
+    it('should remove the "BUY ITEM" buttons', () =>{
+
+    });
+    
+    it('should remove the last three item from the view', () =>{
+
+    });
+    
+    it('should make 10% reduction on all products', () =>{
+
+    });
+    
+    it('should remove the pahts from the katalog', () =>{
+
+    });
+    
+    it('should rename the product shirt to "Fish-Shirt"', () =>{
+
+    });
+    
+    it('should rename the first rock item to "Bird-Rock"', () =>{
+
+    });
+    
+    it('should add 5 stars to the product shirt', () =>{
+
+    });
+    
+    it('should reset the last two images to the url "http://bit.ly/2xq8ev0"', () =>{
+
+    });
+    
+    it('should constantly change the price (#changingPrice), increment it by one in each 3 secs.', () =>{
+
+    });
+    
+    it('should show the "BUY ITEM" again with a green background, gray border and a thin shadow', () =>{
+
+    });
+    
     
 });
 
